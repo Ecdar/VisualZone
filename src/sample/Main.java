456b3b1a874e2a524785b441e5ba684fe318bba3
Unknown <nicolai-brobak@hotmail.com>
2018-03-07 17:39:14 +0100

    Set up basic layout with list of checkboxes and 3D scene

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,24 +1,105 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Shape3D;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application {
 
+    private static ObservableList<Node> dimensionUI;
+    private static ObservableList<Node> zone3DUI;
+    private static Translate cameraPosition;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
         primaryStage.setTitle("Zone Visualization");
-        primaryStage.setScene(new Scene(root, 600, 450));
+        primaryStage.setScene(new Scene(setupScene()));
         primaryStage.show();
+
+        Button testButton = new Button("Test Add");
+        testButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ArrayList<String> dimensionNames = new ArrayList<>();
+                dimensionNames.add("cooldown");
+                dimensionNames.add("work");
+                dimensionNames.add("patience");
+                setDimensions(dimensionNames);
+
+                ArrayList<Shape3D> shapes = new ArrayList<>();
+                Box box = new Box(2, 2, 2);
+                box.setMaterial(new PhongMaterial(Color.RED));
+                box.getTransforms().add(new Translate(4, 4, 4));
+                shapes.add(box);
+                set3DContent(shapes);
+            }
+        });
+        dimensionUI.add(testButton);
     }
 
+    public static Parent setupScene() throws Exception {
+        //Create 3D scene and content
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        cameraPosition = new Translate(5, 5, -10);
+        camera.getTransforms().add(cameraPosition);
+
+        Group subRoot = new Group();
+        zone3DUI = subRoot.getChildren();
+
+        SubScene subScene = new SubScene(subRoot, 640, 480,
+                true, SceneAntialiasing.BALANCED);
+        subScene.setCamera(camera);
+
+        //Create dimension list
+        ScrollPane dimensionScrollPane = new ScrollPane();
+        dimensionScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        dimensionScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        dimensionScrollPane.setPrefWidth(240);
+        dimensionScrollPane.setFitToHeight(true);
+
+        VBox dimensionRoot = new VBox(5);
+        dimensionRoot.setPadding(new Insets(15, 0, 0, 10));
+        dimensionScrollPane.setContent(dimensionRoot);
+        dimensionUI = dimensionRoot.getChildren();
+
+        HBox parent = new HBox(10);
+        parent.getChildren().addAll(dimensionScrollPane, subScene);
+
+        return new Group(parent);
+    }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static void setDimensions(List<String> dimensions) {
+        dimensionUI.clear();
+        for (String dimension : dimensions) {
+            dimensionUI.add(new CheckBox(dimension));
+        }
+    }
+
+    public static void set3DContent(List<Shape3D> content) {
+        zone3DUI.clear();
+        for (Shape3D shape : content) {
+            zone3DUI.add(shape);
+        }
     }
 }
